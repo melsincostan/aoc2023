a731da82fe85d741108a8601dd5ceb07a007fb40
Mélusine Hermier <melusine.hermier@proton.me>
2024-01-26 20:58:53 +0100

    broken implementation for day19 part 2

--- day19/src/part2.rs
+++ day19/src/part2.rs
@@ -1,4 +1,5 @@
-use std::collections::HashMap;
+use std::cmp::{max, min};
+use std::collections::{HashMap, HashSet};
 use std::fs;
 
 #[derive(Debug, PartialEq, Clone)]
@@ -38,7 +39,113 @@ pub fn solve(input: &str) -> u64 {
     // there doesn't seem to be any!! yay!!
     let selected = search(&workflows, first_workflow_name);
     // now to find all distinct combinations!
-    0
+    let collected_ranges = selected.iter().map(reduce).collect::<Vec<HashMap<char, (u64, u64)>>>();
+    let mut merged: HashSet<usize> = HashSet::default();
+    let mut final_ranges: Vec<HashMap<char, (u64, u64)>> = vec![];
+    for i in 0..collected_ranges.len() {
+        if merged.contains(&i) {
+            continue;
+        }
+
+        let mut new_range = collected_ranges[i].to_owned();
+
+        for j in 0..collected_ranges.len() {
+            if j == i || merged.contains(&j) {
+                continue;
+            }
+            let potential_merge = collected_ranges[j].to_owned();
+            let merge_point = can_merge(&new_range, &potential_merge);
+            if merge_point.is_some() {
+                new_range = merge(new_range, potential_merge, &merge_point.unwrap());
+                merged.insert(j);
+            }
+        }
+
+        final_ranges.push(new_range);
+    }
+    final_ranges.iter().for_each(print_range);
+    final_ranges.iter().map(range_value).sum()
+}
+
+fn range_value(r: &HashMap<char, (u64, u64)>) -> u64 {
+    let mut res = 1;
+    for c in vec!['x', 'm', 'a', 's'] {
+        let range = r.get(&c).unwrap();
+        res *= range.1 - range.0;
+    }
+    res
+}
+
+fn merge(a: HashMap<char, (u64, u64)>, b: HashMap<char, (u64, u64)>, on: &char) -> HashMap<char, (u64, u64)> {
+    let a_range = a.get(on).unwrap();
+    let b_range = b.get(on).unwrap();
+    let new_range = (min(a_range.0, b_range.0), max(a_range.1, b_range.1));
+    let mut new_fullrange = a.to_owned();
+    new_fullrange.insert(*on, new_range);
+    new_fullrange
+}
+
+fn print_range(r: &HashMap<char, (u64, u64)>) -> () {
+    for c in vec!['x', 'm', 'a', 's'] {
+        let range = r.get(&c).unwrap();
+        print!("'{}': ({}, {}) ", c, range.0, range.1);
+    }
+    println!("");
+}
+
+fn can_merge(a: &HashMap<char, (u64, u64)>, b: &HashMap<char, (u64, u64)>) -> Option<char> {
+    let mut curr_diff = 0;
+    let mut diff_lett: Option<char> = None;
+    for c in vec!['x', 'm', 'a', 's'] {
+        let a_range = a.get(&c).unwrap();
+        let b_range = b.get(&c).unwrap();
+        if a_range != b_range {
+            curr_diff += 1;
+            if ranges_contiguous(a_range, b_range) {
+                diff_lett = Some(c);
+            }
+            
+        }
+    }
+    if curr_diff == 1 {
+        diff_lett
+    } else {
+        None
+    }
+}
+
+fn ranges_contiguous(a: &(u64, u64), b: &(u64, u64)) -> bool {
+    if a.0 <= b.0 {
+        a.1 >= b.0
+    } else { // implicit b.0 < a.0 
+        b.1 >= a.0
+    }
+}
+
+fn reduce(path: &Vec<Rule>) -> HashMap<char, (u64, u64)> {
+    let mut res: HashMap<char, (u64, u64)> = HashMap::default();
+    res.insert('x', (0, 4000));
+    res.insert('m', (0, 4000));
+    res.insert('a', (0, 4000));
+    res.insert('s', (0, 4000));
+    for rule in path {
+        if rule.condition.is_some() {
+            let current_condition = rule.condition.as_ref().unwrap();
+            let current_range = res.get(&current_condition.tested_value).unwrap();
+            match current_condition.operator {
+                '>' => {
+                    let new_range = (max(current_range.0, current_condition.threshold), current_range.1);
+                    res.insert(current_condition.tested_value, new_range);
+                },
+                '<' => {
+                    let new_range = (current_range.0, min(current_range.1, current_condition.threshold));
+                    res.insert(current_condition.tested_value, new_range);
+                },
+                _ => panic!("Unexpected operator: {}", current_condition.operator)
+            }
+        }
+    }
+    res
 }
 
 fn search(workflows: &HashMap<String, Vec<Rule>>, first_workflow_name: &str) -> Vec<Vec<Rule>> {
@@ -62,7 +169,6 @@ fn search(workflows: &HashMap<String, Vec<Rule>>, first_workflow_name: &str) -> 
             }
         }
     }
-    println!("Total of {} possible paths", selected.len());
     selected
 }
 
